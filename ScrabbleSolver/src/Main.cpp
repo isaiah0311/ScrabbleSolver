d6a16f143072a56052796e9bc809fc16f33e3d43
Isaiah Lateer <isaiahl0311@gmail.com>
2023-02-27 17:33:59 -0500

    dictionary is loaded into a vector of words

--- ScrabbleSolver/src/Main.cpp
+++ ScrabbleSolver/src/Main.cpp
@@ -5,11 +5,50 @@
  * Scrabble word finder
  */
 
+#include <sstream>
 #include <string>
+#include <vector>
 
 #include <windows.h>
 
 #include "Resources.h"
+
+ /**
+  * Loads the dictionary resource file
+  *
+  * Handles locating and reading the dictionary resource file. The data is
+  * loaded into a vector of strings, each element contains a single word
+  * from the dictionary. If the dictionary is empty, an error occurred.
+  *
+  * @param instance is the handle to the program when loaded in memory
+  * @return vector containing dictionary contents
+  */
+std::vector<std::string> loadDictionary(_In_ HINSTANCE instance) {
+	std::vector<std::string> dictionary;
+
+	HRSRC resInfo = FindResourceW(instance, MAKEINTRESOURCEW(ID_DICTIONARY), L"TXT");
+	if (!resInfo)
+		return {};
+
+	HGLOBAL resData = LoadResource(instance, resInfo);
+	if (!resData)
+		return {};
+
+	LPVOID res = LockResource(resData);
+	if (!res)
+		return {};
+
+	DWORD size = SizeofResource(instance, resInfo);
+
+	std::string data;
+	data.assign(static_cast<char*>(res), size);
+	std::stringstream stream(data);
+
+	std::string word;
+	while (std::getline(stream, word))
+		dictionary.push_back(word);
+	return dictionary;
+}
 
 /**
  * Program entry-point
@@ -28,24 +67,16 @@ int WINAPI wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance,
 	_In_ PWSTR cmdLine, _In_ int cmdShow) {
 	AllocConsole();
 
-	HRSRC resInfo = FindResourceW(instance, MAKEINTRESOURCEW(ID_DICTIONARY), L"TXT");
-	if (!resInfo)
+	std::vector<std::string> dictionary = loadDictionary(instance);
+	if (dictionary.empty())
 		return EXIT_FAILURE;
-
-	HGLOBAL resData = LoadResource(instance, resInfo);
-	if (!resData)
-		return EXIT_FAILURE;
-
-	LPVOID res = LockResource(resData);
-	if (!res)
-		return EXIT_FAILURE;
-
-	DWORD size = SizeofResource(instance, resInfo);
 
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-	std::string dictionary = static_cast<char*>(res);
-	WriteConsoleA(out, dictionary.c_str(), size, nullptr, nullptr);
-	WriteConsoleA(out, "\r\n", 2, nullptr, nullptr);
+	for (size_t i = 0; i < dictionary.size(); ++i) {
+		WriteConsoleA(out, dictionary.at(i).c_str(), dictionary.at(i).size(), nullptr, nullptr);
+		WriteConsoleA(out, "\r\n", 2, nullptr, nullptr);
+	}
+
 	system("pause");
 
 	FreeConsole();
