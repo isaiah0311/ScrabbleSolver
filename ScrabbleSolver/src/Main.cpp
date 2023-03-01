2bd68251119879e6e799b35fb10dd9221d81b36f
Isaiah Lateer <isaiahl0311@gmail.com>
2023-02-28 19:07:03 -0500

    takes letters as input and outputs all possible words

--- ScrabbleSolver/src/Main.cpp
+++ ScrabbleSolver/src/Main.cpp
@@ -53,9 +53,9 @@ std::vector<std::string> loadDictionary(_In_ HINSTANCE instance) {
 /**
  * Program entry-point
  *
- * Standard Windows entry-point for C and C++ programs. Allocates a console
- * and then loads the dictionary resource file. Prints the entire Scrabble
- * dictionary and then pauses before exiting.
+ * Standard Windows entry-point for C and C++ programs. Loads the dictionary
+ * resource file and then allocates a console. Prompts the user for their
+ * letters and then outputs all possible words that can be made.
  *
  * @param instance is the handle to the program when loaded in memory
  * @param prevInstance is used for backwards compatability with 16-bit Windows
@@ -65,20 +65,68 @@ std::vector<std::string> loadDictionary(_In_ HINSTANCE instance) {
  */
 int WINAPI wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prevInstance,
 	_In_ PWSTR cmdLine, _In_ int cmdShow) {
-	AllocConsole();
-
 	std::vector<std::string> dictionary = loadDictionary(instance);
 	if (dictionary.empty())
 		return EXIT_FAILURE;
 
+	if (AllocConsole() == ERROR)
+		return EXIT_FAILURE;
+
+	HANDLE in = GetStdHandle(STD_INPUT_HANDLE);
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
-	for (size_t i = 0; i < dictionary.size(); ++i) {
-		WriteConsoleA(out, dictionary.at(i).c_str(), dictionary.at(i).size(), nullptr, nullptr);
-		WriteConsoleA(out, "\r\n", 2, nullptr, nullptr);
+	if (WriteConsoleA(out, "Enter your letters: ", 20, nullptr, NULL) == ERROR)
+		return EXIT_FAILURE;
+
+	char buffer[15] = {};
+	DWORD chars = 0;
+	if (ReadConsoleA(in, buffer, sizeof(buffer), &chars, NULL) == ERROR)
+		return EXIT_FAILURE;
+
+	char letters[26] = {};
+	for (DWORD i = 0; i < chars; ++i) {
+		if (buffer[i] == '\r') {
+			buffer[i] = '\0';
+			chars = i;
+			break;
+		}
+
+		if (buffer[i] >= 'A' && buffer[i] <= 'Z')
+			++letters[buffer[i] - 'A'];
+		else if (buffer[i] >= 'a' && buffer[i] <= 'z')
+			++letters[buffer[i] - 'a'];
+	}
+
+	std::vector<std::string> words;
+	for (std::string word : dictionary) {
+		char frequency[26] = {};
+		for (size_t i = 0; i < word.length(); ++i) {
+			if (word.at(i) >= 'A' && word.at(i) <= 'Z')
+				++frequency[word.at(i) - 'A'];
+			else if (word.at(i) >= 'a' && word.at(i) <= 'z')
+				++frequency[word.at(i) - 'a'];
+		}
+
+		bool valid = true;
+		for (int i = 0; i < 26; ++i) {
+			if (frequency[i] > letters[i]) {
+				valid = false;
+				break;
+			}
+		}
+
+		if (valid)
+			words.push_back(word);
+	}
+
+	for (std::string word : words) {
+		word.append("\r\n");
+		if (WriteConsoleA(out, word.c_str(), word.length(), nullptr, NULL) == ERROR)
+			return EXIT_FAILURE;
 	}
 
 	system("pause");
 
-	FreeConsole();
+	if (FreeConsole() == ERROR)
+		return EXIT_FAILURE;
 	return EXIT_SUCCESS;
 }
